20c076567d450e7157def04b506e5facaa368008
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-06-21 15:59:28 +0000

    #372 - moved ruote bootstrap to singletons
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@518 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/app/singletons/ruote_bootstrap.rb
@@ -0,0 +1,50 @@
+class RuoteBootstrap
+  class << self
+    def init_client
+    end
+    
+    def init_host
+      init_storage
+      RuoteGlobals.host = Host.new(RuoteGlobals.storage)
+      RuoteGlobals.engine = RuoteGlobals.host.engine
+      clear_errors
+      init_workflows
+      register_participants
+      init_job_registry
+    end
+    
+    def init_storage
+      if RuoteConfiguration.use_persistent_storage
+        require 'ruote/storage/fs_storage'
+        storage = Ruote::FsStorage.new('work/xgw')
+      else
+        require 'ruote/storage/hash_storage'
+        storage = Ruote::HashStorage.new
+      end
+      
+      RuoteGlobals.storage = storage
+    end
+    
+    private
+    
+    def clear_errors
+      RuoteGlobals.storage.purge_type!('errors')
+    end
+    
+    def init_workflows
+      RuoteGlobals.workflows = ::ClearspringWorkflows.new
+      RuoteGlobals.workflows.define_workflows
+    end
+    
+    def register_participants
+      engine = RuoteGlobals.engine
+      RuoteGlobals.participants.each do |name, instance|
+        engine.register_participant(name, instance)
+      end
+    end
+    
+    def init_job_registry
+      RuoteGlobals.job_registry = JobRegistry.new
+    end
+  end
+end
