30200ecd90dbfee8739b11af4b7a8c7f84a2273a
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-06-23 23:15:02 +0000

    #347 - fixed gzip splitter spec
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@531 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/facades/gzip_splitter_spec.rb
+++ trunk/spec/facades/gzip_splitter_spec.rb
@@ -8,14 +8,10 @@ require "gzip_splitter"
 
 describe GzipSplitter do
   it "should have a default value for @header_pattern" do
-    GzipSplitter.new("s0400.0.log.gz",:verify => "true").header_pattern.should == "\037\213\b\000\000"
+    GzipSplitter.new(:verify => "true").header_pattern.should == "\037\213\b\000\000"
   end
 
   it "should have a default value for @read_buffer_size" do
-    GzipSplitter.new("s0400.0.log.gz",:verify => "true").read_buffer_size.should == 4096
-  end
-
-  it "should have a value for @input_file" do
-    GzipSplitter.new("s0400.0.log.gz",:verify => "true").input_file.should_not be_nil
+    GzipSplitter.new(:verify => "true").read_buffer_size.should == 4096
   end
 end
