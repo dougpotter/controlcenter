a98ed33c1a430ea04ce4c4ce22ba36cdfefd4ebb
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-07-11 02:44:45 +0000

    #327 - concurrency control for splitting and uploading
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@633 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/participants/s3_participant.rb
+++ trunk/app/participants/s3_participant.rb
@@ -3,14 +3,17 @@ class S3Participant < ParticipantBase
   # s3sync can read ~/.s3conf/s3config.yml where credentials should be stored.
   consume(:upload_file,
     :input => %w(local_path s3_bucket s3_path),
+    :optional_input => %w(lock),
     :sync => true
   ) do
-    local_path = params.input[:local_path]
-    remote_path = "#{params.input[:s3_bucket]}:#{params.input[:s3_path]}"
-    if RuoteConfiguration.verbose_s3
-      debug_print("S3put: #{local_path} -> #{remote_path}")
+    lock_conditionally(params.input[:lock]) do
+      local_path = params.input[:local_path]
+      remote_path = "#{params.input[:s3_bucket]}:#{params.input[:s3_path]}"
+      if RuoteConfiguration.verbose_s3
+        debug_print("S3put: #{local_path} -> #{remote_path}")
+      end
+      Subprocess.spawn_check('s3cmd', 'put', remote_path, local_path)
     end
-    Subprocess.spawn_check('s3cmd', 'put', remote_path, local_path)
   end
   
   private
