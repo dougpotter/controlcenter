61699b7468395af7d09b527da201a809a1b831b3
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-07-11 02:44:08 +0000

    #327 - work in progress on limiting concurrency in workflows
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@627 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/participants/http_participant.rb
+++ trunk/app/participants/http_participant.rb
@@ -1,9 +1,35 @@
 class HttpParticipant < ParticipantBase
   consume(:fetch_directory_listing,
     :input => %w(remote_url),
-    :optional_input => %w(http_username http_password),
+    :optional_input => %w(http_username http_password lock),
     :sync => true
   ) do
+    if lock_name = params.input[:lock]
+      lock(lock_name) do
+        fetch_directory_listing
+      end
+    else
+      fetch_directory_listing
+    end
+  end
+  
+  consume(:fetch_file,
+    :input => %w(remote_url local_path),
+    :optional_input => %w(http_username http_password lock),
+    :sync => true
+  ) do
+    if lock_name = params.input[:lock]
+      lock(lock_name) do
+        fetch_file
+      end
+    else
+      fetch_file
+    end
+  end
+  
+  private
+  
+  def fetch_directory_listing
     output = http_client(
       params.input[:remote_url],
       :http_username => params.input[:http_username],
@@ -12,19 +38,13 @@ class HttpParticipant < ParticipantBase
     params.output.value = output
   end
   
-  consume(:fetch_file,
-    :input => %w(remote_url local_path),
-    :optional_input => %w(http_username http_password),
-    :sync => true
-  ) do
+  def fetch_file
     http_client(
       params.input[:remote_url],
       :http_username => params.input[:http_username],
       :http_password => params.input[:http_password]
     ).download(params.input[:remote_url], params.input[:local_path])
   end
-  
-  private
   
   def http_client(url, options)
     default_options = {}
@@ -33,5 +53,51 @@ class HttpParticipant < ParticipantBase
     end
     options = default_options.update(options)
     ThreadLocalHttpClient.instance(options)
+  end
+  
+  def lock(lock_name)
+    location = Socket.gethostname
+    # todo: make configurable
+    capacity = 1
+    allocation = nil
+    1.upto(101) do |index|
+      begin
+        allocation = Semaphore::Arbitrator.instance.acquire(
+          lock_name, :location => location, :timeout => 30.minutes
+        )
+        break
+      rescue Semaphore::ResourceNotFound
+        # we're missing the resource.
+        # since every host uses their own resource, first time
+        # we run something on a newly provisioned box the resource
+        # is expected to be missing.
+        # create it accounting for other threads racing to
+        # do the same.
+        resource = Semaphore::Resource.new(:name => lock_name, :location => location, :capacity => capacity)
+        begin
+          resource.save!
+        rescue ActiveRecord::RecordInvalid, ActiveRecord::StatementInvalid
+          # see if it already exists
+          if Semaphore::Resource.identity(lock_name, location)
+            break
+          else
+            raise
+          end
+        end
+      rescue Semaphore::ResourceBusy
+        if index == 100
+          raise
+        else
+          # wait
+          sleep 5
+        end
+      end
+    end
+    
+    begin
+      yield
+    ensure
+      Semaphore::Arbitrator.instance.release(allocation)
+    end
   end
 end
