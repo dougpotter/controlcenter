242911cc6792d6e3c5bae199ed29ae52e867cf0d
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-07-20 21:52:31 +0000

    Changed custom validations to not pass configuration to validates_each, since validates_each does not accept such argument
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@664 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/lib/custom_validations.rb
+++ trunk/lib/custom_validations.rb
@@ -6,7 +6,7 @@ module ActiveRecord
         configuration = { :message => "attribute should be of type Date" }
         configuration.update(attr_names.extract_options!)
 
-        validates_each(attr_names, configuration) do |record, attr_name, value|
+        validates_each(*attr_names) do |record, attr_name, value|
           unless value.is_a?(Date)
             record.errors.add(attr_name, :invalid, :default => configuration[:message], :value => value)
           end
@@ -18,7 +18,7 @@ module ActiveRecord
         configuration = { :message => "end date precedes start date" }
         configuration.update(options)
 
-        validates_each(start_attr_name, configuration) do |record, attr_name, value|
+        validates_each(start_attr_name) do |record, attr_name, value|
           if record.send(start_attr_name) > record.send(end_attr_name)
             record.errors.add(start_attr_name, :invalid, :default => configuration[:message], :value => value)
           end
