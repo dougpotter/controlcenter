57d091d9dd1ef16ffa1a67399661d410d90b2995
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-07-20 21:52:20 +0000

    Use is_a? instead of class equality comparison
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@662 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/lib/custom_validations.rb
+++ trunk/lib/custom_validations.rb
@@ -7,7 +7,7 @@ module ActiveRecord
         configuration.update(attr_names.extract_options!)
 
         validates_each(attr_names, configuration) do |record, attr_name, value|
-          unless value.class == Date
+          unless value.is_a?(Date)
             record.errors.add(attr_name, :invalid, :default => configuration[:message], :value => value)
           end
         end
