0217c44ce1c1c48e4662f68d7a39ee20fd9a836b
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-07-21 22:05:50 +0000

    #401 - Fixed bug in seed-fu plugin
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@669 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/vendor/plugins/seed-fu/lib/seed-fu/writer/seed_many.rb
+++ trunk/vendor/plugins/seed-fu/lib/seed-fu/writer/seed_many.rb
@@ -21,6 +21,23 @@ module SeedFu
       def add_seed(hash)
         seed_handle.syswrite( (<<-END
 #{',' unless self.number_of_seeds == 0 or chunk_this_seed?}
+  { #{hash.collect{|k,v| ":#{k} => '#{v.to_s.gsub("'", "\\\\'")}'"}.join(', ')} }
+        END
+        ).chomp )
+        super(hash)
+
+        if chunk_this_seed?
+          seed_handle.syswrite(
+            self.seed_many_footer +
+            "# BREAK EVAL\n" +
+            self.seed_many_header
+          )
+        end
+      end
+=begin
+      def add_seed(hash)
+        seed_handle.syswrite( (<<-END
+#{',' unless self.number_of_seeds == 0 or chunk_this_seed?}
   { #{hash.collect{|k,v| ":#{k} => '#{v.to_s.gsub("'", "\'")}'"}.join(', ')} }
         END
         ).chomp )
@@ -34,7 +51,7 @@ module SeedFu
           )
         end
       end
-
+=end
       def write_header
         super
         seed_handle.syswrite self.seed_many_header
