ef1a03121568d4422684e1059bf45b488778fba8
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-02 21:58:04 +0000

    #401 - updated to use Factory Girl
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@768 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/models/campaign_spec.rb
+++ trunk/spec/models/campaign_spec.rb
@@ -1,91 +1,72 @@
+# == Schema Information
+# Schema version: 20100729211736
+#
+# Table name: campaigns
+#
+#  id             :integer(4)      not null, primary key
+#  description    :text            default(""), not null
+#  campaign_code  :text            default(""), not null
+#  partner_id     :integer(4)
+#  cid            :integer(4)
+#  time_window_id :integer(4)
+#
+
 require 'spec_helper'
+require 'factory_girl'
 
 describe Campaign do
-  before(:each) do
-    @partner_attr = {
-      :name => "name"
-    }
-    @partner = Partner.create!(@partner_attr)
-    @valid_attributes = {
-      :description => "monster campaign",
-      :campaign_code => "XG8100",
-      :start_date => Date.today,
-      :end_date => Date.today + 1,
-      :partner_id => @partner.id,
-      :cid => 1
-    }
-  end
-
   it "should create a new instance given valid attributes" do
-    Campaign.create!(@valid_attributes)
+    Factory.create(:campaign)
   end
 
   it "should require a parent partner" do
     lambda {
-      Campaign.create!(@valid_attributes.merge(:partner_id => @partner.id + 1))
+      Factory.create(:campaign, :partner_id => 100)
     }.should raise_error(ActiveRecord::StatementInvalid)
   end
 
   it "should require non nill description (validations test)" do
     lambda {
-      Campaign.create!(@valid_attributes.merge({:description => nil}))
+      Factory.create(:campaign, :description => nil)
     }.should raise_error
   end
 
   it "should require non nill description (db test)" do
     lambda {
-      c = Campaign.new(@valid_attributes.merge({:description => nil}))
+      c = Factory.build(:campaign, :description => nil)
       c.save_with_validation(false)
     }.should raise_error(ActiveRecord::StatementInvalid)
   end
 
   it "should require non nill campaign code (validations test)" do
     lambda {
-      Campaign.create!(@valid_attributes.merge({:campaign_code => nil}))
+      Factory.create(:campaign, :campaign_code => nil)
     }.should raise_error
   end
 
   it "should require non nill campaign code (db test)" do
     lambda {
-      c = Campaign.new(@valid_attributes.merge({:campaign_code => nil}))
+      c = Factory.build(:campaign, :campaign_code => nil)
       c.save_with_validation(false)
     }.should raise_error(ActiveRecord::StatementInvalid)
   end
 
-  it "should require start_date of type date" do
-    lambda {
-      Campaign.create!(@valid_attributes.merge({:start_date => 3}))
-    }.should raise_error
-  end
-
-  it "should require end_date of type date" do
-    lambda {
-      Campaign.create!(@valid_attributes.merge({:end_date => 3}))
-    }.should raise_error
-  end
-
-  it "should require end date to be >= start date" do
-    lambda {
-      Campaign.create!(@valid_attributes.merge({:end_date => (Date.today - 1)}))
-    }.should raise_error
-  end
-
   it "should require integer partner_id" do
     lambda {
-      Campaign.create!(@valid_attributes.merge({:partner_id => "not and int"}))
+      Factory.create(:campaign, :partner_id => "not and int")
     }.should raise_error
   end
 
   it "should require integer cid" do
     lambda {
-      Campaign.create!(@valid_attributes.merge({:cid => "not and int"}))
+      Factory.create(:campaign, :cid => "not an int")
     }.should raise_error 
   end
 
   it "shoud require unique cid" do
-    Campaign.create!(@valid_attributes)
+    Factory.create(:campaign, :cid => "111")
     lambda {
-      Campaign.create!(@valid_attributes)
+      Factory.create(:campaign, :cid => "111")
     }.should raise_error
   end
 end
