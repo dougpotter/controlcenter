b6eb302ad0ec397743c7c78d1e22b3e8d5d8043e
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-02 22:00:27 +0000

    #401 - gegraphy and time_window specs
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@770 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/spec/models/geography_spec.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+# Schema version: 20100729211736
+#
+# Table name: geographies
+#
+#  id          :integer(4)      not null, primary key
+#  description :string(255)
+#
+
+require 'spec_helper'
+
+describe Geography do
+  before(:each) do
+    @valid_attributes = {
+      :description => "value for description"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Geography.create!(@valid_attributes)
+  end
+end
---
+++ trunk/spec/models/time_window_spec.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+# Schema version: 20100729211736
+#
+# Table name: time_windows
+#
+#  id           :integer(4)      not null, primary key
+#  window_begin :datetime
+#  window_end   :datetime
+#
+
+require 'spec_helper'
+
+describe TimeWindow do
+  it "should create a new instance given valid attributes" do
+    Factory.create(:time_window)
+  end
+
+  it "should require a start date of type Time" do
+    lambda {
+      Factory.create(:time_window, :window_begin => "not a time")
+    }.should raise_error
+  end
+
+  it "should require an end date of type Time" do
+    lambda {
+      Factory.create(:time_window, :window_end => "not a time")
+    }.should raise_error
+  end
+
+  it "sould require end date be after start date" do
+    Factory.build(:time_window, {:window_begin => Time.now + 100, :window_end => Time.now})
+  end
+end
