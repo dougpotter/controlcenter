4004e5a95646946c7557d8e448cc3d122dc45992
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-04 00:05:41 +0000

    Fixed system timer forcing to actually work
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@788 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/lib/force_system_timer.rb
+++ trunk/lib/force_system_timer.rb
@@ -1,11 +1,14 @@
 # make Timeout.timeout call SystemTimer.timeout_after.
 # should only be necessary for ruby 1.8.
 # see http://ph7spot.com/musings/system-timer
+
 require 'system_timer'
 require 'timeout'
+
 module Timeout
-  include SystemTimer
-  def timeout(time, &block)
-    timeout_after(time, &block)
+  def timeout(time, klass=nil, &block)
+    SystemTimer.timeout_after(time, &block)
   end
+  
+  module_function :timeout
 end
