8fccefe03fa7a408642c0da81dd1f9ca20bd6884
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-04 00:05:47 +0000

    #486 - added Net::HTTP and RightHttpConnection http client implementations
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@790 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/app/facades/http_client/net_http.rb
@@ -0,0 +1,61 @@
+require 'net/http'
+require 'uri'
+
+class HttpClient::NetHttp < HttpClient::Base
+  # allowed options:
+  # :http_username
+  # :http_password
+  # :timeout
+  # :debug
+  def initialize(options={})
+    @http_username, @http_password = options[:http_username], options[:http_password]
+    @timeout = options[:timeout]
+    @debug = options[:debug]
+  end
+  
+  def fetch(url)
+    if @debug
+      debug_print "Fetch #{url}"
+    end
+    
+    issue_request(url) do |resp|
+      resp.body
+    end
+  end
+  
+  def download(url, local_path)
+    if @debug
+      debug_print "Download #{url} -> #{local_path}"
+    end
+    
+    issue_request(url) do |resp|
+      File.open(local_path, 'w') do |file|
+        resp.read_body do |chunk|
+          file << chunk
+        end
+      end
+    end
+  end
+  
+  private
+  
+  def issue_request(url)
+    uri = URI.parse(url)
+    req = Net::HTTP::Get.new(uri.path)
+    if @http_username && @http_password
+      req.basic_auth(@http_username, @http_password)
+    end
+    http = Net::HTTP.new(uri.host, uri.port)
+    resp = http.request(req)
+    case resp
+    when Net::HTTPSuccess
+      yield resp
+    else
+      raise HttpClient::HttpError.new(resp.code.to_i, resp.body)
+    end
+  end
+  
+  def debug_print(msg)
+    $stderr.puts(msg)
+  end
+end
---
+++ trunk/app/facades/http_client/right_aws.rb
@@ -0,0 +1,75 @@
+require 'right_http_connection'
+
+class HttpClient::RightAws < HttpClient::Base
+  # allowed options:
+  # :http_username
+  # :http_password
+  # :timeout
+  # :connect_timeout (if :timeout is specified and :connect_timeout is not,
+  #   :timeout is used for :connect_timeout as well)
+  # :debug
+  def initialize(options={})
+    @http_username, @http_password = options[:http_username], options[:http_password]
+    @timeout, @connect_timeout = options[:timeout], options[:connect_timeout]
+    @debug = options[:debug]
+  end
+  
+  def fetch(url)
+    if @debug
+      debug_print "Fetch #{url}"
+    end
+    
+    issue_request(url) do |resp|
+      resp.body
+    end
+  end
+  
+  def download(url, local_path)
+    if @debug
+      debug_print "Download #{url} -> #{local_path}"
+    end
+    
+    issue_request(url) do |resp|
+      File.open(local_path, 'w') do |file|
+        # right http connection breaks read_body
+        file << resp.body
+      end
+    end
+  end
+  
+  private
+  
+  def issue_request(url)
+    options = {
+      :http_connection_read_timeout => @timeout,
+      :http_connection_connect_timeout => @timeout,
+    }
+    if @connect_timeout
+      options[:http_connection_connect_timeout] = @connect_timeout
+    end
+    conn = Rightscale::HttpConnection.new(options)
+    uri = URI.parse(url)
+    req = Net::HTTP::Get.new(uri.path)
+    if @http_username && @http_password
+      req.basic_auth(@http_username, @http_password)
+    end
+    options = {
+      :server => uri.host,
+      :port => uri.port,
+      # right http connection does this, so we don't have to:
+      #:protocol => (uri.port == 443 ? 'https' : 'http'),
+      :request => req,
+    }
+    resp = conn.request(options)
+    case resp
+    when Net::HTTPSuccess
+      yield resp
+    else
+      raise HttpClient::HttpError.new(resp.code.to_i, resp.body)
+    end
+  end
+  
+  def debug_print(msg)
+    $stderr.puts(msg)
+  end
+end
