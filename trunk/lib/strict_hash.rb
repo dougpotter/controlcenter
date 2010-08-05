c6e48f5025edf73b61afbbfa8d94af391b8eae11
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-05 16:06:57 +0000

    #501 - Added optional keys support to strict hashes
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@802 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/lib/strict_hash.rb
+++ trunk/lib/strict_hash.rb
@@ -5,7 +5,19 @@ module StrictHashMixin
     if has_key?(key)
       super
     else
-      raise KeyError, "Key does not exist in hash: #{key}"
+      if @optional_keys && @optional_keys[key]
+        nil
+      else
+        raise KeyError, "Key does not exist in hash: #{key}"
+      end
+    end
+  end
+  
+  def optional_keys(*keys)
+    # same type of access (normal/indifferent) as main hash
+    @optional_keys ||= self.class.new
+    keys.each do |key|
+      @optional_keys[key] = true
     end
   end
 end
