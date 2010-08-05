1a9ce18461821aac5e70cd601d4dc7a93f779e00
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-05 16:06:54 +0000

    Moved workflow module to own file to satisfy rails dependency loading mechanism
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@801 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/app/bases/workflow.rb
@@ -0,0 +1,23 @@
+module Workflow
+  # Base class for workflow errors
+  class WorkflowError < StandardError; end
+  
+  # Another process had begun extracting the requested file.
+  # Extraction may be actively proceeding, or the other process
+  # may have died but its lock timeout had not yet passed.
+  class FileExtractionInProgress < WorkflowError; end
+  
+  # The file had already been extracted with --once option.
+  # This exception is only raised when --once option is given.
+  # Without --once, it is possible to extract the same file
+  # an arbitrary number of times.
+  class FileAlreadyExtracted < WorkflowError; end
+  
+  # Raised when user requests a specific url to be downloaded
+  # and provides date/hour/channel, and the url is actually not
+  # in the specified date/hour/channel.
+  class FileSpecMismatch < WorkflowError; end
+  
+  # Attempting to extract partially uploaded files.
+  class FileNotReady < WorkflowError; end
+end
