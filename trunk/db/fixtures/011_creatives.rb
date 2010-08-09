433775b5fcdbd4d8f5939accce4ddb321185d5c5
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-09 20:34:11 +0000

    #401 - added ais and crid to ad inventory sources and creatives tables respectively
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@831 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/db/fixtures/011_creatives.rb
+++ trunk/db/fixtures/011_creatives.rb
@@ -1,3 +1,3 @@
 Creative.seed_many(:id,[
- {:name => "creative name", :media_type => "media type", :creative_size_id => 1, :campaign_id => 1}
+ {:name => "creative name", :media_type => "media type", :creative_size_id => 1, :campaign_id => 1, :crid => "43KKL"}
 ])
--- trunk/db/fixtures/012_ad_inventory_sources.rb
+++ trunk/db/fixtures/012_ad_inventory_sources.rb
@@ -1,4 +1,4 @@
 AdInventorySource.seed_many(:id,[
- { :id => 1, :name => "AdX" },
- { :id => 2, :name => "Ad Conductor" }
+ { :id => 1, :name => "AdX", :ais => "23KL" },
+ { :id => 2, :name => "Ad Conductor", :ais => "2LKJ6" }
 ])
