067bfa10d04420376de3b2dbdf8f9007120a03b3
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-10 22:23:27 +0000

    #401 - fixed seeds for audience table to accomodate addition of aid
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@856 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/db/fixtures/015_audiences.rb
+++ trunk/db/fixtures/015_audiences.rb
@@ -1,3 +1,3 @@
 Audience.seed_many(:id,[
- {:description => "audience description", :internal_external => "internal", :seed_extraction_id => 1, :model_id => 1}
+ {:description => "audience description", :internal_external => "internal", :seed_extraction_id => 1, :model_id => 1, :aid => "23KJ12"}
 ])
