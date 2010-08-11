e15ed3e67421608ae5e3906dbd73bdc1747e9ea0
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-11 20:02:19 +0000

    Deleted branches/live due to screwup
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@881 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- branches/live/trunk/config/initializers/appwall.rb
+++
@@ -1,7 +0,0 @@
-if File.exist?(File.join( RAILS_ROOT, "config/appwall.yml" )) &&
-    appwall_config = YAML::load(
-      File.open( File.join( RAILS_ROOT, "config/appwall.yml" ) ) )[Rails.env]
-  
-  APPWALL_USERNAME = appwall_config["username"]
-  APPWALL_PASSWORD_HASH = appwall_config["password_hash"]
-end
