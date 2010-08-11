09ea6d2391810d92959fd667ddf7165cbe1db5f8
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-11 14:25:04 +0000

    #511 - corected bug in creative_code column in creatives table
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@871 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/db/fixtures/production/011_creatives.rb
+++ trunk/db/fixtures/production/011_creatives.rb
@@ -1,7 +1,7 @@
 # TODO: This seed list needs to be fleshed out fully.
 
 Creative.seed_many(:id, [
-  { :id => 1, :creative_size_id => 1, :campaign_id => 23, :creative_code => "crid=300v2" },
-  { :id => 2, :creative_size_id => 1, :campaign_id => 31, :creative_code => "crid=300v5" },
-  { :id => 3, :creative_size_id => 2, :campaign_id => 31, :creative_code => "crid=728v5" }
+  { :id => 1, :creative_size_id => 1, :campaign_id => 23, :creative_code => "300v2" },
+  { :id => 2, :creative_size_id => 1, :campaign_id => 31, :creative_code => "300v5" },
+  { :id => 3, :creative_size_id => 2, :campaign_id => 31, :creative_code => "728v5" }
 ])
