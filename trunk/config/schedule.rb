d452c6763027e7091fcbfb618229f50b3ddf44fc
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-16 21:52:15 +0000

    Added a note to schedule.rb explaining how to temporarily disable certain cron jobs
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@931 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/config/schedule.rb
+++ trunk/config/schedule.rb
@@ -1,7 +1,23 @@
-# Use this file to easily define all of your cron jobs.
+# Use this file to define tasks that need to be executed periodically.
+
+# Note: our deployment configuration automatically updates crontab
+# on target system to execute tasks defined in this file (schedule.rb).
+# In order to temporarily prevent a task defined here from running,
+# you can comment out the appropriate line(s) in the target system's
+# crontab. To access crontab for another user, use -u argument to crontab
+# like this:
 #
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# sudo crontab -e -u www
+#
+# Keep in mind that deploying the application will put the tasks back into
+# the target's crontab. If you are deploying code and want to avoid the
+# tasks being put back into the target crontab, comment out the following
+# line in deploy.rb which arranges for the crontab to be updated:
+#
+# after "deploy:symlink", "deploy:update_crontab"
+#
+# This needs to be done in addition to commenting out the entry in remote
+# crontab.
 
 # Clearspring files seem to mostly materialize a couple minutes after each
 # hour mark, with some exceptions. Running extraction exactly on the hour
@@ -9,6 +25,9 @@
 every 1.hour, :at => 45 do
   rake 'workflows:clearspring:autorun'
 end
+
+# It is helpful to understand cron before editing this file:
+# http://en.wikipedia.org/wiki/Cron
 
 # Example:
 #
