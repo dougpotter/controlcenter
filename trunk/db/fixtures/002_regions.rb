ee81ade21b80a8a9daa641f2a25b53b529929dc2
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-16 22:14:45 +0000

    #545 - changed seed_fu to seed by business codes rather than database ids
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@932 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/db/fixtures/002_regions.rb
@@ -0,0 +1,63 @@
+# DO NOT MODIFY THIS FILE, it was auto-generated.
+# 
+# Date: 2010-08-09T16:10:10-04:00
+# Using SeedFu::Writer::SeedMany to seed State
+# Written with the command:
+#
+#   script/generate_geo_seeds 
+#
+State.seed_many(:abbreviation,[
+  { :abbreviation => "VA" },
+  { :abbreviation => "ND" },
+  { :abbreviation => "AL" },
+  { :abbreviation => "NY" },
+  { :abbreviation => "RI" },
+  { :abbreviation => "HI" },
+  { :abbreviation => "DE" },
+  { :abbreviation => "CO" },
+  { :abbreviation => "NE" },
+  { :abbreviation => "MD" },
+  { :abbreviation => "MN" },
+  { :abbreviation => "WY" },
+  { :abbreviation => "MO" },
+  { :abbreviation => "ME" },
+  { :abbreviation => "IA" },
+  { :abbreviation => "PR" },
+  { :abbreviation => "AZ" },
+  { :abbreviation => "KY" },
+  { :abbreviation => "IL" },
+  { :abbreviation => "OR" },
+  { :abbreviation => "OH" },
+  { :abbreviation => "NH" },
+  { :abbreviation => "TN" },
+  { :abbreviation => "GA" },
+  { :abbreviation => "TX" },
+  { :abbreviation => "ID" },
+  { :abbreviation => "SC" },
+  { :abbreviation => "IN" },
+  { :abbreviation => "MS" },
+  { :abbreviation => "CT" },
+  { :abbreviation => "AR" },
+  { :abbreviation => "FL" },
+  { :abbreviation => "NJ" },
+  { :abbreviation => "PA" },
+  { :abbreviation => "MI" },
+  { :abbreviation => "OK" },
+  { :abbreviation => "SD" },
+  { :abbreviation => "MT" },
+  { :abbreviation => "WI" },
+  { :abbreviation => "NV" },
+  { :abbreviation => "VT" },
+  { :abbreviation => "CA" },
+  { :abbreviation => "KS" },
+  { :abbreviation => "DC" },
+  { :abbreviation => "UT" },
+  { :abbreviation => "WV" },
+  { :abbreviation => "MA" },
+  { :abbreviation => "NM" },
+  { :abbreviation => "AK" },
+  { :abbreviation => "NC" },
+  { :abbreviation => "WA" },
+  { :abbreviation => "LA" }
+])
+# End auto-generated file.
--- trunk/db/fixtures/012_ad_inventory_sources.rb
+++ trunk/db/fixtures/012_ad_inventory_sources.rb
@@ -1,6 +1,6 @@
-AdInventorySource.seed_many(:id, :ais_code, [
-  { :id => 1, :name => "Google Ad Exchange (AdX)", :ais_code => "AdX" },
-  { :id => 2, :name => "Burst Backplane", :ais_code => "AdC" },
-  { :id => 3, :name => "OpenX", :ais_code => "OpenX" },
-  { :id => 4, :name => "PAN Inventory Source", :ais_code => "PAN" },
+AdInventorySource.seed_many(:ais_code, [
+  { :name => "Google Ad Exchange (AdX)", :ais_code => "AdX" },
+  { :name => "Burst Backplane", :ais_code => "AdC" },
+  { :name => "OpenX", :ais_code => "OpenX" },
+  { :name => "PAN Inventory Source", :ais_code => "PAN" },
 ])
