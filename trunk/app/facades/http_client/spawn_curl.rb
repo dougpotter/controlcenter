9abb55c057b299d2572de981261082dd3447ff23
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-17 23:13:56 +0000

    #568 - added wget and curl http client implementations that put passwords in process arguments in cleartext
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@962 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/app/facades/http_client/spawn_curl.rb
@@ -0,0 +1,73 @@
+require_dependency 'subprocess'
+
+class HttpClient::SpawnCurl < HttpClient::Base
+  # allowed options:
+  # :command
+  #   Examples:
+  #   :command => 'curl'
+  #   :command => '/usr/bin/curl'
+  #   :command => ['/usr/bin/env', 'curl']
+  # :http_username
+  # :http_password
+  # :timeout
+  # :debug
+  def initialize(options={})
+    @command = options[:command] || %w(/usr/bin/env curl)
+    @http_username, @http_password = options[:http_username], options[:http_password]
+    @timeout = options[:timeout]
+    @debug = options[:debug]
+  end
+  
+  def fetch(url)
+    if @debug
+      debug_print "Fetch #{url}"
+    end
+    
+    cmd = build_command('-o', '-', url)
+    if @debug
+      debug_print "Curl: #{cmd.join(' ')}"
+    end
+    Subprocess.get_output(*cmd)
+  end
+  
+  def download(url, local_path)
+    if @debug
+      debug_print "Download #{url} -> #{local_path}"
+    end
+    
+    cmd = build_command('-o', local_path, url)
+    if @debug
+      debug_print "Curl: #{cmd.join(' ')}"
+    end
+    Subprocess.spawn_check(*cmd)
+  end
+  
+  private
+  
+  def build_command(*args)
+    if @command.is_a?(Array)
+      cmd = @command.dup
+    else
+      cmd = [@command]
+    end
+    if @http_username
+      cmd << '-u'
+      # note that curl claims it will prompt for password if
+      # --user is given and password is not given
+      cmd << "#{@http_username}:#{@http_password}"
+    end
+    if @timeout
+      # curl has no read timeout option, apparently
+      cmd << '--connect-timeout'
+      cmd << @timeout.to_s
+    end
+    unless @debug
+      cmd << '-s'
+    end
+    cmd + args
+  end
+  
+  def debug_print(msg)
+    $stderr.puts(msg)
+  end
+end
---
+++ trunk/app/facades/http_client/spawn_wget.rb
@@ -0,0 +1,74 @@
+require_dependency 'subprocess'
+
+class HttpClient::SpawnWget < HttpClient::Base
+  # allowed options:
+  # :command
+  #   Examples:
+  #   :command => 'wget'
+  #   :command => '/usr/bin/wget'
+  #   :command => ['/usr/bin/env', 'wget']
+  # :http_username
+  # :http_password
+  # :timeout
+  # :debug
+  def initialize(options={})
+    @command = options[:command] || %w(/usr/bin/env wget)
+    @http_username, @http_password = options[:http_username], options[:http_password]
+    @timeout = options[:timeout]
+    @debug = options[:debug]
+  end
+  
+  def fetch(url)
+    if @debug
+      debug_print "Fetch #{url}"
+    end
+    
+    cmd = build_command('-O', '-', url)
+    if @debug
+      debug_print "Wget: #{cmd}"
+    end
+    Subprocess.get_output(*cmd)
+  end
+  
+  def download(url, local_path)
+    if @debug
+      debug_print "Download #{url} -> #{local_path}"
+    end
+    
+    cmd = build_command('-O', local_path, url)
+    if @debug
+      debug_print "Wget: #{cmd}"
+    end
+    Subprocess.spawn_check(*cmd)
+  end
+  
+  private
+  
+  def build_command(*args)
+    if @command.is_a?(Array)
+      cmd = @command.dup
+    else
+      cmd = [@command]
+    end
+    if @http_username
+      cmd << '--user'
+      cmd << @http_username
+    end
+    if @http_password
+      cmd << '--password'
+      cmd << @http_password
+    end
+    if @timeout
+      cmd << '--timeout'
+      cmd << @timeout.to_s
+    end
+    unless @debug
+      cmd << '-q'
+    end
+    cmd + args
+  end
+  
+  def debug_print(msg)
+    $stderr.puts(msg)
+  end
+end
