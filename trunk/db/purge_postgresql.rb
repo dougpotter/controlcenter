81e7ae191b91fd1b89045baa7a2d1e50a81d45a4
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-20 06:10:47 +0000

    #583 - added ability to have a custom database purging script
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@989 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/db/purge_postgresql.rb
@@ -0,0 +1,7 @@
+abcs = ActiveRecord::Base.configurations
+ActiveRecord::Base.establish_connection(abcs['test'])
+connection = ActiveRecord::Base.connection
+connection.tables.each do |table|
+  connection.execute("drop table #{connection.quote_table_name(table)}")
+end
+
