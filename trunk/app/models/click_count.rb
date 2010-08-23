b15411e98222ff5d8124f719978b4f7307ad9876
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-23 21:15:14 +0000

    #520 - added business_objects method to click_counts
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1016 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/models/click_count.rb
+++ trunk/app/models/click_count.rb
@@ -27,6 +27,10 @@ class ClickCount < ActiveRecord::Base
   validates_presence_of :campaign_id, :creative_id, :ad_inventory_source_id, :audience_id, :start_time, :end_time, :duration_in_minutes, :click_count
   validates_numericality_of :click_count
   validates_as_increasing :start_time, :end_time, :allow_nil => false
+  
+  def business_objects
+    [ campaign, creative, ad_inventory_source, audience ]
+  end
 
   def business_attributes
     ["start_time", "end_time", "duration_in_minutes", "campaign_code", "creative_code", "ad_inventory_source_code", "geography", "audience_code"]
