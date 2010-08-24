30b89856dd0d72ab78ed0e3d4f5bb3aa54ce45db
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-24 17:04:23 +0000

    #447 - fixed listing check to work correctly, accounting for file name differences caused by gzip splitting
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1020 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/workflows/clearspring_verify_workflow.rb
+++ trunk/app/workflows/clearspring_verify_workflow.rb
@@ -13,7 +13,7 @@ class ClearspringVerifyWorkflow < ClearspringExtractWorkflow
       remote_relative_path = url_to_relative_data_source_path(url)
       local_path = build_local_path(remote_relative_path)
       bucket_path = build_s3_path(local_path)
-      if our_paths.include?(bucket_path)
+      if extracted_paths_include?(our_paths, bucket_path)
         puts "Have #{bucket_path}"
       else
         puts "Missing #{bucket_path}"
@@ -34,5 +34,16 @@ class ClearspringVerifyWorkflow < ClearspringExtractWorkflow
   
   def list_bucket_items
     @s3_client.list_bucket_files(s3_bucket)
+  end
+  
+  # due to gzip splitting our paths do not necessarily correspond exactly to
+  # data source paths. our paths may contain a suffix distinguishing one
+  # split file from another
+  def extracted_paths_include?(our_paths, their_path)
+    their_path_prefix = their_path.sub(/\.log\.gz$/, '')
+    our_paths.any? do |our_path|
+      # this test is a little sketchy but it will work for v1
+      our_path[0...their_path_prefix.length] == their_path_prefix
+    end
   end
 end
