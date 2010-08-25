cb070881d6bb1ba404f2705183069b95f2d431cb
macreery <macreery@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-25 17:04:05 +0000

    Fixed name of Pandora AIS
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1033 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/db/fixtures/012_ad_inventory_sources.rb
+++ trunk/db/fixtures/012_ad_inventory_sources.rb
@@ -2,5 +2,5 @@ AdInventorySource.seed_many(:ais_code, [
   { :name => "Google Ad Exchange (AdX)", :ais_code => "AdX" },
   { :name => "Burst Backplane", :ais_code => "AdC" },
   { :name => "OpenX", :ais_code => "OpenX" },
-  { :name => "PAN Inventory Source", :ais_code => "PAN" },
+  { :name => "Pandora", :ais_code => "PAN" },
 ])
