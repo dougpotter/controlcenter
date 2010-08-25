a1764c0ce0875e5b4224572c728511ce83041c52
macreery <macreery@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-25 21:53:16 +0000

    #558 - Fix creatives seed
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1058 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/db/fixtures/production/011_creatives.rb
+++ trunk/db/fixtures/production/011_creatives.rb
@@ -1,159 +1,74 @@
 # TODO: This seed list needs to be fleshed out fully.
 
-Creative.seed_many(:campaign_id, :creative_code, [
+Creative.seed_many(:creative_code, [
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("BY51").id,
-    :creative_code => "300p"
+    :creative_code => "16122:300p",
+    :creative_size => CreativeSize.find_by_height_and_width(300, 250),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "BY51" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("BY51").id,
-    :creative_code => "300x600"
+    :creative_code => "16122:300v2",
+    :creative_size => CreativeSize.find_by_height_and_width(300, 250),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "E929", "F50B" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("E929").id,
-    :creative_code => "300v2"
+    :creative_code => "16122:300v5",
+    :creative_size => CreativeSize.find_by_height_and_width(300, 250),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "UCCO" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("E929").id,
-    :creative_code => "728v6"
+    :creative_code => "16122:300x600",
+    :creative_size => CreativeSize.find_by_height_and_width(300, 250),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "BY51" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("F50B").id,
-    :creative_code => "300v2"
+    :creative_code => "16122:728v2",
+    :creative_size => CreativeSize.find_by_height_and_width(728, 90),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "F50B" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("F50B").id,
-    :creative_code => "300v2"
+    :creative_code => "16122:728v5",
+    :creative_size => CreativeSize.find_by_height_and_width(728, 90),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "UCCO", "ZT4R" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("F50B").id,
-    :creative_code => "300v2"
+    :creative_code => "16122:728v6",
+    :creative_size => CreativeSize.find_by_height_and_width(728, 90),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "E929" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("F50B").id,
-    :creative_code => "728v2"
+    :creative_code => "16122:728v8",
+    :creative_size => CreativeSize.find_by_height_and_width(728, 90),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "ZT4R" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("F50B").id,
-    :creative_code => "728v2"
+    :creative_code => "18182:160bs",
+    :creative_size => CreativeSize.find_by_height_and_width(160, 600),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "D8JD", "8LM7" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("F50B").id,
-    :creative_code => "728v2"
+    :creative_code => "18182:160ew",
+    :creative_size => CreativeSize.find_by_height_and_width(160, 600),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "D8JD" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("UCCO").id,
-    :creative_code => "300v5"
+    :creative_code => "18182:300bs",
+    :creative_size => CreativeSize.find_by_height_and_width(300, 250),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "D8JD", "8LM7" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("UCCO").id,
-    :creative_code => "300v5"
+    :creative_code => "18182:300ew",
+    :creative_size => CreativeSize.find_by_height_and_width(300, 250),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "D8JD", "8LM7" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("UCCO").id,
-    :creative_code => "300v5"
+    :creative_code => "18182:728bs",
+    :creative_size => CreativeSize.find_by_height_and_width(728, 90),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "8LM7", "D8JD" ] ]) 
   },
   {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("UCCO").id,
-    :creative_code => "728v5"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("UCCO").id,
-    :creative_code => "728v5"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("UCCO").id,
-    :creative_code => "728v5"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("ZT4R").id,
-    :creative_code => "728v5"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("ZT4R").id,
-    :creative_code => "728v8"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(160, 600),
-    :campaign_id => Campaign.find_by_campaign_code("8LM7").id,
-    :creative_code => "160bs"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("8LM7").id,
-    :creative_code => "300bs"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("8LM7").id,
-    :creative_code => "300bs"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("8LM7").id,
-    :creative_code => "300ew"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("8LM7").id,
-    :creative_code => "728bs"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("8LM7").id,
-    :creative_code => "728bs"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("8LM7").id,
-    :creative_code => "728ew"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(160, 600),
-    :campaign_id => Campaign.find_by_campaign_code("D8JD").id,
-    :creative_code => "160bs"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(160, 600),
-    :campaign_id => Campaign.find_by_campaign_code("D8JD").id,
-    :creative_code => "160ew"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("D8JD").id,
-    :creative_code => "300bs"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(300, 250),
-    :campaign_id => Campaign.find_by_campaign_code("D8JD").id,
-    :creative_code => "300ew"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("D8JD").id,
-    :creative_code => "728bs"
-  },
-  {
-    :creative_size_id => CreativeSize.find_by_height_and_width(728, 90),
-    :campaign_id => Campaign.find_by_campaign_code("D8JD").id,
-    :creative_code => "728ew"
+    :creative_code => "18182:728ew",
+    :creative_size => CreativeSize.find_by_height_and_width(728, 90),
+    :campaigns => Campaign.find(:all, :conditions => ["campaign_code IN (?)", [ "8LM7", "D8JD" ] ]) 
   }
 ])
