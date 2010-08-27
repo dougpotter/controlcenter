a2fc43416527c928e88e6cb650372f01df379880
xgraph <xgraph@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-08-27 22:05:19 +0000

    #552 - Refactoring goals for FactsController
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1086 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- branches/factrefactor/app/controllers/facts_controller.rb
+++ branches/factrefactor/app/controllers/facts_controller.rb
@@ -1,180 +1,62 @@
-require "yaml"
-
 class FactsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-
-  attr_accessor :attrs
-  attr_accessor :fact_class
-
+  skip_before_filter :verify_authenticity_token, :only => [:update, :index]
+  
   def index
-    @csv_rows = []
-    @end_time = (Time.parse(params[:end_time]) rescue (Date.today - 1.day))
-    @start_time = (Time.parse(params[:start_time]) rescue (@end_date - 7.day))
-
-    render :text => nil, :status => 501 unless params[:frequency] = "hour" 
-
-    code_ids = {
-      :campaign_code => "campaign_id",
-      :creative_code => "creative_id",
-      :ais_code => "ad_inventory_source_id",
-      :audience_code => "audience_id",
-      :mpm_code => "media_purchase_method_id"
-    }
-    
-    code_classes = {
-      :campaign_code => Campaign,
-      :creative_code => Creative,
-      :ais_code => AdInventorySource,
-      :audience_code => Audience,
-      :mpm_code => MediaPurchaseMethod
-    }
-
-    group_by_dimensions = params[:dimensions].split(",")
-    group_by_columns = []
-    group_by_dimensions.each do |dim|
-      if code_ids[dim.to_sym]
-        group_by_columns << code_ids[dim.to_sym]
-      else
-        group_by_dimensions.delete(dim)
-      end
-    end
-    group_by_columns.concat(["start_time", "end_time"])
-    group_by_clause = group_by_columns.collect { |col|
-      "#{ImpressionCount.connection.quote_column_name(col)}"
-    }.join(", ")
-
-    where_array = params.select { |k, v| 
-      !([ :metrics, :dimensions, :start_time, :end_time, :frequency, :action, :controller, :format ].include?(k.to_sym)) 
-    }
-    where_clause = (where_array.collect { |dim|
-      code_ids[dim[0].to_sym]
-    }.compact.collect { |col|
-      "#{ImpressionCount.connection.quote_column_name(col)} = ?"
-    }.join(" AND "))
-    
-    where_values = where_array.collect { |row|
-      code_classes[row[0].to_sym] ? code_classes[row[0].to_sym].send(:handle_to_id, row[1]) : nil
-    }.compact
-
-    grouped_sql_hash = {}
-    params[:metrics].split(",").each_with_index do |fact_name, idx|
-      fact_sum_term = "SUM(#{ImpressionCount.connection.quote_column_name(fact_name)})"
-      table_name = ImpressionCount.connection.quote_table_name(fact_name.pluralize)
-      grouped_sql_result = ImpressionCount.find_by_sql([
-        "SELECT #{group_by_clause}, #{fact_sum_term} " +
-        "FROM #{table_name} WHERE start_time >= ? AND " +
-        "end_time <= ? #{where_clause.empty? ? "" : "AND"} " +
-        "#{where_clause} GROUP BY #{group_by_clause}"
-      ].concat([@start_time, @end_time]).concat(where_values))
-
-      grouped_sql_result.each do |row|
-        grouped_attr_array = []
-        group_by_dimensions.each do |group_dim|
-          grouped_attr_array <<
-            row.send(code_ids[group_dim.to_sym].gsub("_id", "")).send(group_dim)
-        end
-        grouped_attr_array.concat([row.start_time, row.end_time])
-        grouped_sql_hash[grouped_attr_array] ||= []
-        grouped_sql_hash[grouped_attr_array][idx] = row.attributes[fact_sum_term]
-      end
-    end
-    
-    facts = params[:metrics].split(",")
-    
-    @csv_rows = []
-    @csv_rows[0] = []
-    @csv_rows[0].concat(group_by_dimensions)
-    @csv_rows[0].concat(["start_time", "end_time"])
-    @csv_rows[0].concat(facts)
-    
-    grouped_sql_hash.each do |k, v|
-      @csv_rows << k.concat(v)
-    end
+    # Returns object of type FactAggregation
+    @fact_aggregation = Fact.aggregate({
+      :include => params[:metrics].split(","),
+      :group_by => params[:dimensions].split(""),
+      :where => where_conditions_from_params,
+      :frequency => params[:frequency]
+    })
     
     respond_to do |format|
-      format.csv do 
-        render_csv("#{facts.join("-")}" +
-                   "#{@start_time.strftime("%Y%m%d")}-#{@end_time.strftime("%Y%m%d")}")
+      format.html
+      format.csv do
+        @csv_rows = @fact_aggregation.to_csv
+        render_csv("facts.csv")  # TODO: Codify name (#616)
       end
     end
   end
 
   def create
-    discern_facts_present
-    for fact in @facts_present
-      @attrs = {}
-      @fact_class = ActiveRecord.const_get(fact.to_s.classify)
-      fill_attrs
-      @fact_class.create!(@attrs)
-    end
-
-    render :text => nil, :status => 200
+    create_many
   end
-
-  def update
-    discern_facts_present
-    for fact in @facts_present
-      @attrs = {}
-      @fact_class = ActiveRecord.const_get(fact.to_s.classify)
-      fill_attrs
-      lookup_attrs = {} 
-      @attrs.select { |k, v| v != nil && v != "" && k.to_s != fact.to_s }.each do |el|
-        lookup_attrs[el[0]] = el[1]
+  
+  def create_many
+    fact_classes_from_params.each do |fact_class|
+      fact_class.new(params)
+      if fact_class.save!
+        render :text => nil, :status => 200
+      else
+        render :text => nil, :status => 422
       end
-      facts = @fact_class.find(:all, :conditions => lookup_attrs)
-      facts[0].send("#{fact}=", facts[0].send(fact) + @attrs[fact].to_f) if facts.size == 1 && params[:operation] == "increment"
-      facts[0].save!
     end
-
-    render :text => nil, :status => 200
   end
-
-  def new
-    @fact_class = ActiveRecord.const_get(params[:table_name].classify)
-    required_dimensions = @fact_class.new.business_attributes
-    render :text => { "required_dimensions" => required_dimensions }.to_yaml
+  
+  def update
+    update_many
+  end
+  
+  def update_many
+    fact_classes_from_params.each do |fact_class|
+      all_facts = fact_class.find_all_by_dimensions(:conditions => params)
+      if all_facts.size == 1
+        all_facts[0].update(params)
+        if all_facts[0].save!
+          render :text => nil, :status => 200
+        else
+          render :text => nil, :status => 422
+        end
+      else
+        render :text => nil, :status => 404  # TODO: Update spec doc
+    end
   end
 
   private
-  def discern_facts_present
-    @facts_present = []
-    all_facts = [:impression_count, :click_count]
-    for fact in all_facts
-      @facts_present << fact if params.member?(fact)
-    end
+  def fact_classes_from_params
   end
-
-  def fill_attrs
-    parse_time 
-    parse_geography
-    fill_fks
-    fill_fact_value
-  end
-
-  def parse_time
-    @attrs[:start_time] = params.delete(:start_time)
-    @attrs[:end_time] = params.delete(:end_time)
-    @attrs[:duration_in_minutes] = params.delete(:duration_in_minutes)
-  end
-
-  def parse_geography
-    @attrs[:geography_id] = params.delete(:geography)
-  end
-
-  def fill_fks
-    fact_fks = @fact_class.new.attributes.keys
-    fact_fks.each do |fk|
-      if fk.match(/.*_id/)
-        fk_class = ActiveRecord.const_get(fk.to_s[0..-4].classify)
-        handle = fk_class.new.get_handle
-        fk_value = fk_class.handle_to_id(params[handle])
-        @attrs[fk.to_sym] = fk_value
-      end
-    end
-  end
-
-  def fill_fact_value
-    fact_as_sym = @fact_class.to_s.underscore.to_sym
-    @attrs[fact_as_sym] = params[fact_as_sym]
+  
+  def where_conditions_from_params
   end
 end
