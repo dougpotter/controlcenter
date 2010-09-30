7867c9a9a1c6026757c81a4239dc82aa2d4368a2
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-09-30 23:34:36 +0000

    Made quote_identifier available on active record classes as well
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1401 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/lib/active_record_extensions.rb
+++ trunk/lib/active_record_extensions.rb
@@ -1,15 +1,25 @@
 module ActiveRecordExtensions
   def self.included(base)
     base.class_eval do
+      extend ClassMethods
       include InstanceMethods
     end
   end
   
-  module InstanceMethods
+  module Quoting
     # A shorter alias for quote_table_name
     def quote_identifier(name)
       connection.quote_table_name(name)
     end
+  end
+  
+  module ClassMethods
+    include Quoting
+  end
+  
+  module InstanceMethods
+    include Quoting
+    private :quote_identifier
   end
 end
 
