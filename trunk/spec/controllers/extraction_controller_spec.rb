5c28c9e941887deef5462afe8a8a557d22fd298c
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-10-04 17:25:42 +0000

    #688 - started writing tests for extraction status
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1415 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/spec/controllers/extraction_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe ExtractionController do
+  # Requesting extraction status when no data exists for the day in question
+  # should not produce errors. Use a day far in the past for checking.
+  it "should not produce errors when status for a day with no data is requested" do
+    date = '20000228'
+    get 'status', :date => date
+    response.should be_success
+  end
+end
