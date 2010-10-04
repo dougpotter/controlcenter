b8ebc8e79a48decb962a15d65b948e8bfcdadae9
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-10-04 20:59:56 +0000

    Mergec [1424] into live branch
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1425 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- branches/live/db/fixtures/012_ad_inventory_sources.rb
+++ branches/live/db/fixtures/012_ad_inventory_sources.rb
@@ -6,5 +6,6 @@ AdInventorySource.seed_many(:ais_code, [
   { :name => "Google Ad Exchange (AdX) - CNT", :ais_code => "AdX-CNT" },
   { :name => "Burst Backplane - CNT", :ais_code => "AdC-CNT" },
   { :name => "OpenX - CNT", :ais_code => "OX-CNT" },
-  { :name => "AppNexus", :ais_code => "ApN" }
+  { :name => "AppNexus", :ais_code => "ApN" },
+  { :name => "AppNexus - CNT", :ais_code => "ApN-CNT"}
 ])
