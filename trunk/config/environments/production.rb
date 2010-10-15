b350bbd00ee5a82553ddea25fd20742103041b5c
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-10-15 00:49:33 +0000

    Improved error reporting on missing haml by referencing Sass explicitly in global scope
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1469 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/config/environments/production.rb
+++ trunk/config/environments/production.rb
@@ -27,5 +27,5 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-Sass::Plugin.options[:never_update] = true
-Sass::Plugin.options[:style] = :compressed
+::Sass::Plugin.options[:never_update] = true
+::Sass::Plugin.options[:style] = :compressed
