ed5d1cdb25135dd0ea4ced6739bc2f3c5c4914a2
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-10-15 00:52:42 +0000

    Bumped required rspec verison to 1.3.0 since we use some of the assertions introduced in that version
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1475 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/config/environments/test.rb
+++ trunk/config/environments/test.rb
@@ -32,7 +32,7 @@ ENV['AUTOFEATURE'] = "true"
 ENV['RSPEC'] = "true"
 
 # Manage RubyGems
-config.gem "rspec", :lib => false, :version => ">= 1.2.0"
+config.gem "rspec", :lib => false, :version => ">= 1.3.0"
 config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
 config.gem "factory_girl", :lib => false, :version => ">= 1.3.1"
 
