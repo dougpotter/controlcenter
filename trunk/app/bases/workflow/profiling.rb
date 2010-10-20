1d3274b5d88a1c9cb1ca616e4751438fee748add
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-10-20 18:40:46 +0000

    #457 - moved Workflow* classes into Workflow module - indentation changes
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1537 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/bases/workflow/profiling.rb
+++ trunk/app/bases/workflow/profiling.rb
@@ -1,43 +1,45 @@
-module Workflow::Profiling
-  def start_profiling
-    # delay loading ruby-prof until profiling is actually needed
-    begin
-      RubyProf
-    rescue NameError
-      require 'ruby-prof'
-    end
-    
-    RubyProf.measure_mode = RubyProf::PROCESS_TIME 
-    RubyProf.start
-  end
-
-  def stop_profiling
-    result = RubyProf.stop
-    puts 'Saving graph html view'
-    printer = RubyProf::GraphHtmlPrinter.new(result)
-    printer.print(File.open('profile-output.html', 'w'), :min_percent => 1)
-  end
-
-  def with_optional_profiling(enable_profiling)
-    if enable_profiling
-      start_profiling
-      
+module Workflow
+  module Profiling
+    def start_profiling
+      # delay loading ruby-prof until profiling is actually needed
       begin
-        status = yield
-      ensure
+        RubyProf
+      rescue NameError
+        require 'ruby-prof'
+      end
+      
+      RubyProf.measure_mode = RubyProf::PROCESS_TIME 
+      RubyProf.start
+    end
+
+    def stop_profiling
+      result = RubyProf.stop
+      puts 'Saving graph html view'
+      printer = RubyProf::GraphHtmlPrinter.new(result)
+      printer.print(File.open('profile-output.html', 'w'), :min_percent => 1)
+    end
+
+    def with_optional_profiling(enable_profiling)
+      if enable_profiling
+        start_profiling
+        
         begin
-          stop_profiling
+          status = yield
+        ensure
+          begin
+            stop_profiling
+          rescue Exception => e
+            status = ConsoleUi.handle_unhandled_exception(e)
+          end
+        end
+      else
+        begin
+          status = yield
         rescue Exception => e
           status = ConsoleUi.handle_unhandled_exception(e)
         end
       end
-    else
-      begin
-        status = yield
-      rescue Exception => e
-        status = ConsoleUi.handle_unhandled_exception(e)
-      end
+      status
     end
-    status
   end
 end
--- trunk/app/bases/workflow/runner.rb
+++ trunk/app/bases/workflow/runner.rb
@@ -1,78 +1,80 @@
-class Workflow::Runner
-  include Workflow::Profiling
-  
-  attr_reader :logger
-  
-  def run(params)
-    @logger = create_logger(params)
-    if @logger
-      # need to save this before running the workflow because the workflow
-      # may override process title
-      @script_name = File.basename($0)
+module Workflow
+  class Runner
+    include Workflow::Profiling
+    
+    attr_reader :logger
+    
+    def run(params)
+      @logger = create_logger(params)
+      if @logger
+        # need to save this before running the workflow because the workflow
+        # may override process title
+        @script_name = File.basename($0)
+        
+        log_marker("#{self.class.name} run started at #{Time.now}")
+      end
       
-      log_marker("#{self.class.name} run started at #{Time.now}")
+      default_params = HashWithIndifferentAccess.new(:logger => @logger)
+      params = default_params.update(params)
+      perform(params)
+      log_marker("#{self.class.name} run finished successfully at #{Time.now}")
+      0
+    rescue Exception => exc
+      raise if Interrupt === exc || SystemExit === exc
+      rv = handle_exception(exc)
+      log_marker("#{self.class.name} run died at #{Time.now}")
+      rv
     end
     
-    default_params = HashWithIndifferentAccess.new(:logger => @logger)
-    params = default_params.update(params)
-    perform(params)
-    log_marker("#{self.class.name} run finished successfully at #{Time.now}")
-    0
-  rescue Exception => exc
-    raise if Interrupt === exc || SystemExit === exc
-    rv = handle_exception(exc)
-    log_marker("#{self.class.name} run died at #{Time.now}")
-    rv
-  end
-  
-  def prepare(params)
-    if params[:system_timer]
-      require 'force_system_timer'
-    end
-  end
-  
-  def exec(params)
-    status = with_optional_profiling(params[:profile]) do
-      run(params)
+    def prepare(params)
+      if params[:system_timer]
+        require 'force_system_timer'
+      end
     end
     
-    exit(status)
-  end
-  
-  private
-  
-  def create_logger(options)
-    if (path = options[:debug_output_path]) && path != '-'
-      Logger.new(path)
-    else
-      nil
+    def exec(params)
+      status = with_optional_profiling(params[:profile]) do
+        run(params)
+      end
+      
+      exit(status)
     end
-  end
-  
-  def handle_exception(exc)
-    forward_unhandled_exception(exc)
-    1
-  end
-  
-  def forward_unhandled_exception(exc, options={})
-    options = {
-      :cc_logger => @logger,
-      :progname => @script_name,
-    }.update(options)
-    ConsoleUi.handle_unhandled_exception(exc, options)
-  end
-  
-  def report_error(message)
-    STDERR.puts(message)
-    if @logger
-      @logger.error(@script_name) { message }
+    
+    private
+    
+    def create_logger(options)
+      if (path = options[:debug_output_path]) && path != '-'
+        Logger.new(path)
+      else
+        nil
+      end
     end
-  end
-  
-  def log_marker(message)
-    if @logger
-      @logger.info(@script_name) do
-        message
+    
+    def handle_exception(exc)
+      forward_unhandled_exception(exc)
+      1
+    end
+    
+    def forward_unhandled_exception(exc, options={})
+      options = {
+        :cc_logger => @logger,
+        :progname => @script_name,
+      }.update(options)
+      ConsoleUi.handle_unhandled_exception(exc, options)
+    end
+    
+    def report_error(message)
+      STDERR.puts(message)
+      if @logger
+        @logger.error(@script_name) { message }
+      end
+    end
+    
+    def log_marker(message)
+      if @logger
+        @logger.info(@script_name) do
+          message
+        end
       end
     end
   end
