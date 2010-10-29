6dc8580b08ca88a97feb163c92a7161a87de1b46
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-10-29 00:35:44 +0000

    Merged [1612:1615] from trunk to live
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1616 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- branches/live/app/workflows/akamai_cleanup_workflow.rb
+++ branches/live/app/workflows/akamai_cleanup_workflow.rb
@@ -15,6 +15,10 @@ class AkamaiCleanupWorkflow < Workflow::CleanupBase
     channels = data_provider.data_provider_channels.all(:order => 'name')
     channels.each do |channel|
       dir = source_dir_for_channel(channel)
+      # XXX ugly hack to get cleanup functional for initial release.
+      # What should happen is a separate workflow should be instantiated
+      # for every channel, like all other workflows work.
+      @params[:channel] = channel
       cleanup_dir(dir, params)
     end
   end
@@ -24,7 +28,18 @@ class AkamaiCleanupWorkflow < Workflow::CleanupBase
   # Returns the timestamp of path for cleanup purposes. We take the end
   # of range that the file encompasses.
   def determine_file_time(path)
-    date, start_hour, end_hour = date_and_hours_from_path(path)
+    begin
+      date, start_hour, end_hour = date_and_hours_from_path(path)
+    rescue ArgumentError
+      if params[:debug]
+        debug_print("Unable to determine file time from path: #{path}")
+      end
+      # XXX this is a moderately evil hack to avoid removing files that
+      # do not look like they are part of ETL process.
+      # Return current time, assuming only files of some age are removed
+      # this file should be kept alone
+      return Time.now.utc
+    end
     # here we assume file timestamps are in UTC
     year, month, day = date[0..3].to_i, date[4..5].to_i, date[6..7].to_i
     # end_hour may be 24, use arithmetic instead of passing end_hour to new
