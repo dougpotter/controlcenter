9648f899bff1b21a82ba0231ec1e6113ccd6d7e0
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-11-05 05:30:55 +0000

    Added tests for identifying time range endpoints in akamai workflows
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1690 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/workflows/akamai_access_spec.rb
+++ trunk/spec/workflows/akamai_access_spec.rb
@@ -24,4 +24,25 @@ describe AkamaiAccess do
     s3_prefix = @wrapper.build_s3_prefix('/root/akamai/xgraph_f.201010310000-2400.0.log.gz')
     s3_prefix.should == '42/raw/20101031'
   end
+  
+  it "should properly identify time range endpoints for daily updated files" do
+    date, start_hour, end_hour = @wrapper.date_and_hours_from_name('xgraph_f.201010310000-2400.0.log.gz')
+    date.should == '20101031'
+    start_hour.should == 0
+    end_hour.should == 24
+  end
+  
+  it "should properly identify time range endpoints for hourly updated files" do
+    date, start_hour, end_hour = @wrapper.date_and_hours_from_name('xgraph_f.201010302000-2100.0.log.gz')
+    date.should == '20101030'
+    start_hour.should == 20
+    end_hour.should == 21
+  end
+  
+  it "should properly identify time range endpoints for four-hourly updated files" do
+    date, start_hour, end_hour = @wrapper.date_and_hours_from_name('xgraph_f.201010310000-2400.0.log.gz')
+    date.should == '20101031'
+    start_hour.should == 0
+    end_hour.should == 24
+  end
 end
