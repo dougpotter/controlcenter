3f149ac2b2e986c37c5e3c83cb67078f5db10f56
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-11-08 20:22:35 +0000

    #877 - removed new partner creation from new campaign page
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1708 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/helpers/campaign_management_helper.rb
+++ trunk/app/helpers/campaign_management_helper.rb
@@ -2,6 +2,10 @@ module CampaignManagementHelper
   def new_partner_option
     "<option value=''>New partner</option>"
   end
+
+  def blank_partner_option
+    "<option value=''>-</option>"
+  end
   
   def all_select_option
     "<option value=''>All</option>"
