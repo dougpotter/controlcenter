72206f5f3a73840c2bc5a88cbf46c3b3a73b3ec7
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-11-13 02:49:05 +0000

    #878 - better testing for creatives controller
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1740 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/controllers/creatives_controller_spec.rb
+++ trunk/spec/controllers/creatives_controller_spec.rb
@@ -2,18 +2,41 @@ require 'spec_helper'
 
 describe CreativesController, "create with valid attributes" do
 
-  def do_create
-    post :create, :creative => { :creative_size => "1", :description => "desc", :media_type => "flash", :creative_code => "ACODE", :campaigns => "1" }
+  before :each do
+    @creative = mock_model(Creative)
+    @creative_size = CreativeSize.new
+    @campaign = mock_model(Campaign)
+    @creative.should_receive(:creative_size_id=).with("1")
+    @creative.should_receive(:attributes=).with({"media_type"=>"flash", "creative_code"=>"ACODE", "description"=>"desc"})
+    @creative.should_receive(:save).and_return(true)
   end
 
-  it "should save the new creative" do
-    creative = Creative.new
-    creative_size = CreativeSize.new
-    campaign = Campaign.new
-    CreativeSize.expects(:find).with("1").returns(creative_size)
-    Campaign.expects(:find).with("1").returns(campaign)
-    Creative.expects(:new).with({"creative_code" => "ACODE", "description" => "desc", "media_type" => "flash"}).returns(creative)
-    creative.expects(:save).returns(true)
-    do_create
+  it "should save a new creative associated with one campaign" do
+    @creative.should_receive(:campaigns).with().and_return(@campaign)
+    @campaign.should_receive(:<<).with(@campaign)
+    CreativeSize.expects(:find).with("1").returns(@creative_size)
+    Campaign.expects(:find).with("1").returns(@campaign)
+    Creative.expects(:new).returns(@creative)
+
+    post :create, :creative => { :creative_size => "1", :description => "desc", :media_type => "flash", :creative_code => "ACODE", :campaigns => "1" }
+
   end
+
+  it "should save a new creative associated with multiple campaigns" do
+    @creative.should_receive(:campaigns).with().and_return(@campaign)
+    @campaign.should_receive(:<<).with(@campaign)
+    CreativeSize.expects(:find).with("1").returns(@creative_size)
+    Campaign.expects(:find).with(["2","1"]).returns(@campaign)
+    Creative.expects(:new).returns(@creative)
+
+    post :create, :creative => { :creative_size => "1", :description => "desc", :media_type => "flash", :creative_code => "ACODE", :campaigns => ["2","1"] }
+  end
+
+  it "should save a new creative not yet associated with any campaigns" do
+    CreativeSize.expects(:find).with("1").returns(@creative_size)
+    Creative.expects(:new).returns(@creative)
+
+    post :create, :creative => { :creative_size => "1", :description => "desc", :media_type => "flash", :creative_code => "ACODE" }
+  end
+
 end
