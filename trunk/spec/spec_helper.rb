261929d147ca2a974cb391c2080bdecf8e319b9d
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-12-07 17:07:24 +0000

    Declare usage of mocha in control center specs
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@1970 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/spec_helper.rb
+++ trunk/spec/spec_helper.rb
@@ -50,7 +50,8 @@ Spec::Runner.configure do |config|
   # RSpec uses it's own mocking framework by default. If you prefer to
   # use mocha, flexmock or RR, uncomment the appropriate line:
   #
-  # config.mock_with :mocha
+  # http://mentalized.net/journal/2008/09/11/rspec_and_mocha_lesson_learned/
+  config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
   #
