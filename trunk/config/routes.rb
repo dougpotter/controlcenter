dd5ad188cf422f53d6d8908b30495dc0e6249be8
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-12-16 23:21:39 +0000

    #1043 - new rout for line items
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2016 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -38,7 +38,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect "/audiences/index_by_advertiser", :controller => "audiences", :action => "index_by_advertiser"
 
-  map.connect "/creatives/form_without_campaign", :controller => "creatives", :action => "form_without_campaign"
+  map.connect "/creatives/form_without_line_item", :controller => "creatives", :action => "form_without_line_item"
   
   # Facts are known on the outside as "metrics"
   map.resources :facts, :as => "metrics"
@@ -59,6 +59,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :partners
   map.resources :ad_inventory_sources
   map.resources :creatives
+  map.resources :line_items
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "landing_pages",
