38c724a2356e34a62e164c8bf5b97b887e959b82
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-12-17 02:27:34 +0000

    Moved paperclip gem declaration into the right place
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2022 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/config/environment.rb
+++ trunk/config/environment.rb
@@ -46,6 +46,8 @@ Rails::Initializer.run do |config|
   # kills the app.
   gem 'haml'
 
+  config.gem 'paperclip'
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -64,6 +66,4 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  
-  config.gem 'paperclip'
 end
