01ca31e87cc19cf9d5de547265b34d0a2f617423
macreery <macreery@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-12-20 23:20:14 +0000

    #1060 - Alter AddLineItemIdToCampaigns migration
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2069 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/db/migrate/20101214215151_add_line_item_id_to_campaigns.rb
+++ trunk/db/migrate/20101214215151_add_line_item_id_to_campaigns.rb
@@ -1,20 +1,53 @@
+require "md5"
+
 class AddLineItemIdToCampaigns < ActiveRecord::Migration
+  def self.codify_identifier(str, length = 5)
+    MD5::md5(str.to_s).to_s[0..(length-1)].upcase
+  end
+  
   def self.up
-    # if line item and campaign tables are empty, just add the column (as non-null
-    # and a foreign key)
-    if LineItem.all == [] && Campaign.all == []
-      add_column :campaigns, :line_item_id, :integer, :null => false
-      add_foreign_key :campaigns, :line_items
+
+    add_column :campaigns, :line_item_id, :integer, :null => false
     
-    # if not, add an unknown partner and unknown line item and assign all existing
-    # campaigns to those two (to be properly classified later) 
-    else 
-      Partner.create({:name => "Unclassified Partner", :partner_code => 9999999})
-      LineItem.create({ :line_item_code => 9999999, :name => "Unclassified Line Item", :partner_id => Partner.find(:first, :conditions => { :partner_code => 9999999 }).id }) 
-      add_column :campaigns, :line_item_id, :integer, :null => false, :default => LineItem.find(:first, :conditions => { :line_item_code => 9999999 })
-      add_foreign_key :campaigns, :line_items
+    num_campaigns = connection.select_value(
+      "SELECT COUNT(*) FROM campaigns;").to_i
+    
+    # if campaign table is empty, just add the column (as non-null
+    # and a foreign key)
+    if num_campaigns > 0
+      partner_ids = connection.select_values(
+        "SELECT DISTINCT partner_id FROM campaigns;").collect { |id| id.to_i }
+      
+      partner_ids.each do |partner_id|
+        partner_name = connection.select_value(
+          "SELECT name FROM partners WHERE " +
+          "id = #{quote(partner_id)} LIMIT 1;")
+        partner_code = connection.select_value(
+          "SELECT partner_code FROM partners WHERE " +
+          "id = #{quote(partner_id)} LIMIT 1;")
+        default_line_item_code = codify_identifier(partner_code)
+        default_line_item_name = "Default Line Item for #{partner_name}"
+        
+        # Assign all unattached campaigns to a "default" line item
+        unless (inserted_row_id = connection.select_value(
+          "SELECT id FROM line_items WHERE line_item_code = " + 
+          "#{quote(default_line_item_code)} LIMIT 1;"))
+          execute("INSERT INTO line_items (line_item_code, name, partner_id) " +
+            "VALUES (#{quote(default_line_item_code)}, " + 
+            "#{quote(default_line_item_name)}, " + 
+            "#{partner_id});")
+          inserted_row_id = connection.select_value(
+            "SELECT id FROM line_items WHERE line_item_code = " + 
+            "#{quote(default_line_item_code)} LIMIT 1;")
+        end
+        
+        execute("UPDATE campaigns SET line_item_id = " + 
+          "#{quote(inserted_row_id)} WHERE " +
+          "partner_id = #{quote(partner_id)};")
+      end
     end
 
+    add_foreign_key :campaigns, :line_items
 
   end
 
