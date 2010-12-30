fd5f8440b1a361392a8c1e8e2ef159f4ca61ec02
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2010-12-30 19:59:52 +0000

    removing erroneous migration
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2172 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/db/migrate/20101230052708_remove_campaign_source_id_from_campaigns.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCampaignSourceIdFromCampaigns < ActiveRecord::Migration
-  def self.up
-    remove_column :campaigns, :campaign_source_id
-  end
-
-  def self.down
-    add_column :campaigns, :campaign_source_id, :integer, :null => false
-  end
-end
