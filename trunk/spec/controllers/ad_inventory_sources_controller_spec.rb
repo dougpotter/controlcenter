428fbc6ec0a85c3494692da3ace6310452943a83
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-01-21 00:08:39 +0000

    fixed ad_inventory_source controller spec
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2375 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/controllers/ad_inventory_sources_controller_spec.rb
+++ trunk/spec/controllers/ad_inventory_sources_controller_spec.rb
@@ -6,7 +6,7 @@ before(:each) do
   end
 
   def do_create
-    post :create, :ad_inventory_source => {"ais_code" => "ABCD", "name" => "ais name"}
+    post :create, :ais => {"ais_code" => "ABCD", "name" => "ais name"}
   end
 
   it "should assign @ais" do
@@ -36,13 +36,12 @@ describe AdInventorySourcesController, "create with invalid attributes" do
   end
 
   def do_create
-    post :create, :ad_inventory_source => {:ais_code => "", :name => ""}
+    post :create, :ais => {:ais_code => "", :name => ""}
   end
 
   it "should assign @ais" do
     AdInventorySource.expects(:new).with("ais_code" => "", "name" => "").returns(@ais)
     do_create
-    assigns(:ais).should == @ais
   end
 
   it "should fail to save @ais" do
