ad0ec9d9adbbb1909a2bb670533c032d8e4f857f
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-02-07 22:11:40 +0000

    #1182 - changes to creatives helper to reflect corrected creative size seeds
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2563 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/helpers/creatives_helper.rb
+++ trunk/app/helpers/creatives_helper.rb
@@ -4,9 +4,9 @@ module CreativesHelper
     when "Medium Rectangle"
       image_tag creative.image.url, :width => 150
     when "Leaderboard"
-      image_tag creative.image.url, :width => 100
-    when "Skyscraper"
       image_tag creative.image.url, :height => 80
+    when "Skyscraper"
+      image_tag creative.image.url, :width => 100
     end
   end
 end
