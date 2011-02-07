9b6ab6990281e59e09064eef196df48659c1cbe1
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-02-07 22:08:29 +0000

    corrected creative size seeds
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2562 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/db/fixtures/009_creative_sizes.rb
+++ trunk/db/fixtures/009_creative_sizes.rb
@@ -2,7 +2,7 @@
 #   http://www.iab.net/iab_products_and_industry_services/1421/1443/1452
 
 CreativeSize.seed_many(:height, :width, [
-  { :height => 300, :width => 250, :common_name => "Medium Rectangle" },
-  { :height => 728, :width => 90, :common_name => "Leaderboard" },
-  { :height => 160, :width => 600, :common_name => "Skyscraper" },
+  { :width => 300, :height => 250, :common_name => "Medium Rectangle" },
+  { :width => 728, :height => 90, :common_name => "Leaderboard" },
+  { :width => 160, :height => 600, :common_name => "Wide Skyscraper" },
 ])
