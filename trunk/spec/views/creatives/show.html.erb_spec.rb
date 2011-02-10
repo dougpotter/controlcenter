f7baa78abe0a8c83cfd67ed17b462e1bd83b068c
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-02-10 18:26:08 +0000

    #1197 - spec for creative show view
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2593 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/spec/views/creatives/show.html.erb_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "creatives/show.html.erb" do
+  it "should render" do
+    image = mock(
+      "image",
+      :url => "http://apath"
+    )
+    creative_size = mock(
+      "creative_size",
+      :height_width_string => "widthXheight"
+    )
+    ais = mock(
+      "ais",
+      :name => "ais name"
+    )
+    ais.expects(:ais_code).times(3).returns("ACODE", "ACODE", "ACODE")
+    caic = mock(
+      "campaign_inventory_config",
+      :ad_inventory_source => ais
+    )
+    campaign = mock(
+      "campaign",
+      :name => "campaign name",
+      :campaign_inventory_configs => [ caic ]
+    )
+    creative = mock(
+      "creative",
+      :name => "creative name",
+      :image => image,
+      :creative_code => "ACODE",
+      :media_type => "type_of_media",
+      :creative_size => creative_size,
+      :campaigns => [ campaign ],
+      :configured? => true
+    )
+    creative.expects(:ae_pixels).times(3).returns(
+      "http://pixel", 
+      "http://pixel",
+      "http://pixel"
+    )
+    assigns[:creative] = creative
+    render
+  end
+end
