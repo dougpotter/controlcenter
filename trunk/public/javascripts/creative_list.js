d03f2f40f097f671cf9c30c61162bc18a8336bfe
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-03-02 17:39:58 +0000

    minor enhancement to javascript
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2745 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/public/javascripts/creative_list.js
+++ trunk/public/javascripts/creative_list.js
@@ -37,7 +37,7 @@ var removeCreativeForm = function(creative_num) {
   var creative_entry_forms = $$('.creative_form_without_line_item');
   var number_of_creatives = $$('.creative_form_without_line_item').length;
   var creative_to_be_removed = $('creative_form_without_line_item[' + creative_num + ']');
-  creative_to_be_removed.setProperty('text', '');
+  creative_to_be_removed.dispose();
 }
 
 window.addEvent('domready', function() {
