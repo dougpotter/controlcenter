55da011459ea39df5775885f25380048b2cd3b56
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-03-02 17:40:44 +0000

    fixed s3_xguid_list_prefix validation regex, wasn't matching valid prefixes
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2746 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/models/appnexus_sync_parameters.rb
+++ trunk/app/models/appnexus_sync_parameters.rb
@@ -23,7 +23,7 @@ class AppnexusSyncParameters < ActiveRecord::Base
   column :s3_xguid_list_prefix, :string
   validates_presence_of :s3_xguid_list_prefix
   # subdir/filename
-  validates_format_of :s3_xguid_list_prefix, :with => %r(\A[a-zA-Z0-9\-]+:[a-zA-Z0-9\-_./]+/[a-zA-Z0-9\-_.]+\Z)
+  validates_format_of :s3_xguid_list_prefix, :with => %r(\A[a-zA-Z0-9\-]+:[a-zA-Z0-9\-_.\/]+\/[a-zA-Z0-9\-_.]+\Z)
   validates_each :s3_xguid_list_prefix do |record, attr, value|
     if value =~ %r|^https?://|
       record.errors.add(:s3_xguid_list_prefix, 'Looks like a URL - it should be an S3 prefix instead')
