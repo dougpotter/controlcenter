e8b969411c338370a357e4d8783f7e1088da4af7
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-03-08 19:53:30 +0000

    #1245 - view spec for advertiser destroy
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2793 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/spec/views/partners/edit.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "/partners/edit.html.erb" do
+  it "should render" do
+    @partner = stub_everything("Partner")
+    template.expects(:error_messages_for).returns(nil)
+    @partner.expects(:class).times(4).returns(Partner)
+    assigns[:partner] = @partner
+    render
+  end
+
+  it "should contain Delete Advertiser button" do
+    @partner = stub_everything("Partner", :id => 1)
+    template.expects(:error_messages_for).returns(nil)
+    @partner.expects(:class).times(4).returns(Partner)
+    assigns[:partner] = @partner
+    render
+    response.should have_tag("form[action=/partners/#{@partner.id}]") do
+      with_tag "input[value=Delete Advertiser]"
+    end
+  end
+end
