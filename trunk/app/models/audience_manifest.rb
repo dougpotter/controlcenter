d0e6c07e7fbae9f8a50f6384a3ade03adfb84e66
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-03-15 20:36:05 +0000

    #1283 - audience manifest specs
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2894 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/models/audience_manifest.rb
+++ trunk/app/models/audience_manifest.rb
@@ -1,2 +1,5 @@
 class AudienceManifest < ActiveRecord::Base
+  validates_presence_of :audience_id, 
+    :audience_source_id, 
+    :audience_iteration_number
 end
--- trunk/spec/models/audience_manifest_spec.rb
+++ trunk/spec/models/audience_manifest_spec.rb
@@ -4,4 +4,45 @@ describe AudienceManifest do
   it "should create new instance given valid attributes" do
     Factory.create(:audience_manifest)
   end
+
+  it "should not allow duplicate audience id - audience source id combinations" do
+    a1 = Factory.build(:audience_manifest)
+    audience_id = a1.audience_id
+    audience_source_id = a1.audience_source_id
+    a1.save
+    a2 = Factory.build(
+      :audience_manifest, 
+      :audience_id => audience_id, 
+      :audience_source_id => audience_source_id
+    )
+    lambda {
+      a2.save
+    }.should raise_error(ActiveRecord::StatementInvalid)
+  end
+
+  it "should faild to create new instance with missing audience id (validation)" do
+    lambda {
+      Factory.create(:audience_manifest, :audience_id => nil)
+    }.should raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "should faild to create new instance with missing audience id (db)" do
+    lambda {
+      a = Factory.build(:audience_manifest, :audience_id => nil)
+      a.save(false)
+    }.should raise_error(ActiveRecord::StatementInvalid)
+  end
+
+  it "should faild to create new instance with missing audience source id (validation)" do
+    lambda {
+      Factory.create(:audience_manifest, :audience_source_id => nil)
+    }.should raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "should faild to create new instance with missing audience source id (db)" do
+    lambda {
+      a = Factory.build(:audience_manifest, :audience_source_id => nil)
+      a.save(false)
+    }.should raise_error(ActiveRecord::StatementInvalid)
+  end
 end
