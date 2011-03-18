3e1a45c9e9acab5181237602bcfc61e05f8ccdd4
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-03-18 19:49:55 +0000

    step definitions for audience
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@2934 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/features/step_definitions/audience_steps.rb
+++ trunk/features/step_definitions/audience_steps.rb
@@ -12,3 +12,18 @@ end
 Then /^I should see the following audiences:$/ do |expected_audiences_table|
   expected_audiences_table.diff!(tableish('table tr', 'td,th'))
 end
+
+Given /^the standard partner, line item, and campaign exist$/ do
+  When "the following partners:", table(%{
+    | partner_code | name |
+    |     11111    | Ford |
+  })
+  When "the following line_items:", table(%{
+    | line_item_code |    name     | partner_code |
+    |     ABC1       | Ford Spring |    11111     |   
+  })
+  When "the following campaigns:", table(%{
+   | campaign_code | line_item_code |     name      |
+   |     ACODE     |     ABC1       | Ford Campaign |
+  })
+end
