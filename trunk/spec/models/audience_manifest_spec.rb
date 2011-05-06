1076bba262c576defe83e5c683a28ef0cc8dc507
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-05-06 20:22:43 +0000

    #1403 - relaxed unique audience_id audience_source_id constraint in audience manifest spec
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@3243 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/models/audience_manifest_spec.rb
+++ trunk/spec/models/audience_manifest_spec.rb
@@ -5,21 +5,6 @@ describe AudienceManifest do
     Factory.create(:audience_manifest)
   end
 
-  it "should not allow duplicate audience id - audience source id combinations" do
-    a1 = Factory.build(:audience_manifest)
-    audience_id = a1.audience_id
-    audience_source_id = a1.audience_source_id
-    a1.save
-    a2 = Factory.build(
-      :audience_manifest, 
-      :audience_id => audience_id, 
-      :audience_source_id => audience_source_id
-    )
-    lambda {
-      a2.save
-    }.should raise_error(ActiveRecord::StatementInvalid)
-  end
-
   it "should fail to create new instance with missing audience id (validation)" do
     a = Factory.build(:audience_manifest, :audience_id => nil)
     a.should have(1).error_on(:audience_id)
