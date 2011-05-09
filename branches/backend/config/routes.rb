210a31056049a03bd6781715890797e55f6a39d4
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-05-09 22:39:46 +0000

    #1397 - fixed root display
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@3268 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- branches/backend/config/routes.rb
+++ branches/backend/config/routes.rb
@@ -30,28 +30,6 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
   
-  map.metrics_report '/home/metrics/report', :controller => 'landing_pages', :action => 'report'
-  map.metrics_home "/home/metrics", :controller => "landing_pages", :action => "metrics" 
-  map.update_form "/home/metrics/update_form", :controller => "landing_pages", :action => "update_form"
-
-  map.connect "/creatives/new_creative_line", :controller => "creatives", :action => "new_creative_line"
-
-  map.connect "/audiences/index_by_advertiser", :controller => "audiences", :action => "index_by_advertiser"
-
-  map.connect "/creatives/form_without_line_item", :controller => "creatives", :action => "form_without_line_item"
-
-  map.connect "/audiences/audience_source_form", :controller => "audiences", :action => "audience_source_form"
-
-  
-  # Facts are known on the outside as "metrics"
-  map.resources :facts, :as => "metrics"
-  # Also allow for accessing Facts#update without :id, per API spec
-  map.connect "/metrics", :controller => "facts", :action => "update", 
-    :conditions => { :method => :put }
-  
-  map.campaign_management_index '/campaign_management', 
-    :controller => "campaign_management", :action => 'index'
-
   map.extraction_index '/extraction',
     :controller => 'extraction', :action => 'index'
   map.extraction_overview '/extraction/overview/:year/:month',
@@ -68,17 +46,9 @@ ActionController::Routing::Routes.draw do |map|
   map.appnexus_sync '/appnexus/sync/:id',
     :controller => 'appnexus', :action => 'show'
 
-  map.resources :audiences
-  map.resources :partners
-  map.resources :ad_inventory_sources
-  map.resources :creatives
-  map.resources :line_items
-  map.resources :campaigns
-  
-
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "landing_pages",
-           :action => "metrics"
+  map.root :controller => "extraction",
+           :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
