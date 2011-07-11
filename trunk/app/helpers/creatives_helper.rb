46aefde727bd289cb4bd55c810512f441bf062df
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-07-11 18:38:28 +0000

    creative image now appears next to edit form on edit campaign page
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@3564 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/helpers/creatives_helper.rb
+++ trunk/app/helpers/creatives_helper.rb
@@ -9,4 +9,15 @@ module CreativesHelper
       image_tag creative.image.url, :width => 100
     end
   end
+
+  def image_tag_for_show(creative)
+    case creative.creative_size.common_name
+    when "Medium Rectangle"
+      image_tag creative.image.url, :width => 100
+    when "Leaderboard"
+      image_tag creative.image.url, :height => 58
+    when "Skyscraper"
+      image_tag creative.image.url, :width => 66
+    end
+  end
 end
