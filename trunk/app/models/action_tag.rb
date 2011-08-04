90417540bcaa8326b48cc69db84eebf1dd8412ac
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-08-04 16:29:48 +0000

    #1523 - should have gone in with [3709]
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@3710 54744ef3-1e76-4ba4-9b02-fe248b4192f5

---
+++ trunk/app/models/action_tag.rb
@@ -0,0 +1,3 @@
+class ActionTag < ActiveRecord::Base
+  belongs_to :partner
+end
---
+++ trunk/public/javascripts/partner_setup.js
@@ -0,0 +1,5 @@
+window.addEvent('domready', function() {
+  $('add_action_tag').addEvent('click', function() {
+    alert("This should add a new action tag form");
+    });
+})
