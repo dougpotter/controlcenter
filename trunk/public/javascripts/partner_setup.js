8415f14a6581582ea03c981d3455100091ad053b
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-08-08 13:53:33 +0000

    #1523 - added minus icon to action tag forms
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@3723 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/public/javascripts/partner_setup.js
+++ trunk/public/javascripts/partner_setup.js
@@ -2,5 +2,12 @@ var appendActionTagForm = function(rawFormMarkup) {
   var newFormIndex = $$('.action_tag_form').length;
   var cleanFormMarkup = setNestedFormIndex(rawFormMarkup, newFormIndex);
   $('action_tags_forms').grab(Elements.from(cleanFormMarkup)[0]);
+
+  $$('.action_tag_minus_sign').each(function(minus_icon, index) {
+    minus_icon.removeEvents();
+    minus_icon.addEvent('click', function(e) { 
+      e.stop();
+      alert("Should remove the form") });
+  });
 }
 
