fb9c1438a25e7826d2baddc01627396fb1168974
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-08-10 20:47:21 +0000

    #1524 - minor reword
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@3764 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/controllers/partners_controller_spec.rb
+++ trunk/spec/controllers/partners_controller_spec.rb
@@ -1,61 +1,66 @@
 require 'spec_helper'
 
-describe PartnersController, "create partner only with valid attributes" do
+describe PartnersController, "create partner with valid attributes" do
+  context "and no action tags or conversion pixels" do
+    before(:each) do
+      @partner = mock("Partner", :save => true, :save_apn => true, :name => "name")
+      Partner.expects(:new).returns(@partner)
+    end
 
-  before(:each) do
-    @partner = mock("Partner", :save => true, :save_apn => true, :name => "name")
-    Partner.expects(:new).returns(@partner)
+    after(:each) do
+      Partner.delete_all_apn
+    end
+
+    def do_create
+      post :create, :partner => {
+        "partner_code" => "12345", 
+        "name" => "partner name" }
+    end
+
+    it "should save @partner at xgcc and apn" do
+      do_create
+    end
+
+    it "should be redirect" do
+      do_create
+      response.should redirect_to(new_partner_url)
+    end
   end
 
-  after(:each) do
-    Partner.delete_all_apn
-  end
-
-  def do_create
-    post :create, :partner => {
-      "partner_code" => "12345", 
-      "name" => "partner name" }
-  end
-
-  it "should save @partner at xgcc and apn" do
-    do_create
-  end
-
-  it "should be redirect" do
-    do_create
-    response.should redirect_to(new_partner_url)
+  context "and action tags with valid attributes and no conversion pixels" do
   end
 end
 
 describe PartnersController, "create partner only with invalid attributes" do
+  context "and no action tags or conversion pixels" do
+    before(:each) do
+      @partner = mock("Partner", :save => false)
+      Partner.expects(:new).returns(@partner)
+    end
 
-  before(:each) do
-    @partner = mock("Partner", :save => false)
-    Partner.expects(:new).returns(@partner)
+    def do_create
+      post :create, :partner => {:partner_code => "", :name => ""}
+    end
+
+    it "should fail to save @partner" do
+      do_create
+    end
+
+    it "should render new action with errors" do
+      do_create
+      response.should render_template(:new)
+    end
   end
 
-  def do_create
-    post :create, :partner => {:partner_code => "", :name => ""}
-  end
+  describe PartnersController, "destroy" do
 
-  it "should fail to save @partner" do
-    do_create
-  end
+    it "should call destroy on the partner whose id is passed in params[:id]"
 
-  it "should render new action with errors" do
-    do_create
-    response.should render_template(:new)
-  end
-end
-
-describe PartnersController, "destroy" do
-
-  it "should call destroy on the partner whose id is passed in params[:id]"
-
-  it "should redirect to new partner page" do
-    pending
-    @partner = Factory.create(:partner)
-    delete :destroy, :id => @partner.id
-    response.should redirect_to(new_partner_url)
+    it "should redirect to new partner page" do
+      pending
+      @partner = Factory.create(:partner)
+      delete :destroy, :id => @partner.id
+      response.should redirect_to(new_partner_url)
+    end
   end
 end
