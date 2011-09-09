c8aff8a18a9a27c9abd79b0861a0c8b11b2fb760
oleg <oleg@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-09-09 17:22:26 +0000

    #1565 - shorten emr log url labels in appnexus bulk loading ui
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@3883 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- branches/backend/app/helpers/appnexus_helper.rb
+++ branches/backend/app/helpers/appnexus_helper.rb
@@ -2,4 +2,22 @@ module AppnexusHelper
   def appnexus_status_name(status)
     %w(None Created Processing Completed Failed)[status]
   end
+  
+  def shorten_emr_log_file_name(path)
+    segments = path.split('/')
+    segments.map! do |segment|
+      if segment =~ /^(.+?)((?:\..{2,5})+$)/
+        segment, ext = $1, $2
+      else
+        ext = ''
+      end
+      
+      if segment.length > 14
+        segment = segment[0...6] + '..' + segment[-7...-1]
+      end
+      
+      segment + ext
+    end
+    segments.join('/')
+  end
 end
