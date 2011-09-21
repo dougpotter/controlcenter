a17acee9cebbd530e2b6204db6cb6f1e3e35fae4
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-09-21 19:52:15 +0000

    remove delete link
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@3995 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/spec/views/partners/edit.html.erb_spec.rb
+++ trunk/spec/views/partners/edit.html.erb_spec.rb
@@ -3,20 +3,10 @@ require 'spec_helper'
 describe "/partners/edit.html.erb" do
   it "should render" do
     @partner = stub_everything("Partner", :class => Partner)
-    @partner.expects(:id).times(4).returns(1)
+    @partner.expects(:id).times(3).returns(1)
     template.expects(:error_messages_for).returns(nil)
     @partner.expects(:class).times(4).returns(Partner)
     assigns[:partner] = @partner
     render
-  end
-
-  it "should contain Delete Advertiser button" do
-    @partner = stub_everything("Partner", :class => Partner)
-    @partner.expects(:id).times(5).returns(1)
-    template.expects(:error_messages_for).returns(nil)
-    @partner.expects(:class).times(4).returns(Partner)
-    assigns[:partner] = @partner
-    render
-    response.should have_tag("a[href=/partners/#{@partner.id}]")
   end
 end
