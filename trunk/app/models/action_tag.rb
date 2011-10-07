9360b787436b48c4ccd5109715f0dc275a7b77ef
michael <michael@54744ef3-1e76-4ba4-9b02-fe248b4192f5>
2011-10-07 19:26:04 +0000

    #1651 - url can now be blank and contain text that is not a url
    git-svn-id: https://dev.xgraph.net/svn/xgraph/controlcenter@4095 54744ef3-1e76-4ba4-9b02-fe248b4192f5

--- trunk/app/models/action_tag.rb
+++ trunk/app/models/action_tag.rb
@@ -1,10 +1,9 @@
 class ActionTag < ActiveRecord::Base
   belongs_to :partner
 
-  validates_presence_of :name, :sid, :url, :partner_id
+  validates_presence_of :name, :sid, :partner_id
   validates_uniqueness_of :sid
   validates_numericality_of :sid, :greater_than => 9999, :less_than => 100000
-  validates_format_of_url :url
 
   before_validation :url_encode_name
 
